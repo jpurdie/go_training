d8e797156f018f73790b5c0b3eeb06bbb8a8f602
Justin Purdie <justindpurdie@gmail.com>
2022-02-14 22:46:16 -0700

    channels

--- gobyexample/channels/main.go
+++ gobyexample/channels/main.go
@@ -2,23 +2,109 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"time"
 )
 
+/*
+Channels are a typed conduit through which you can send and receive values with the channel operator, <-
+*/
 func main() {
 	fmt.Println("Begin")
 
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Enter a val")
+		return
+	}
+	switch args[0] {
+	case "1":
+		runOne()
+	case "2":
+		runTwo()
+	case "3":
+		runThree()
+	case "4":
+		runFour()
+	case "5":
+		runFive()
+	}
+
+}
+
+func runFive() {
 	done := make(chan string)
 	fmt.Println("Main going to call hello go goroutine 1")
-	go hello(1, 3, done)
+	go hello(1, 5, done)
+	fmt.Println("Main going to call hello go goroutine 2")
+	go hello(2, 1, done)
+	// removing "channel receivers" as a test
+
+}
+
+func runFour() {
+	done := make(chan string)
+	fmt.Println("Main going to call hello go goroutine 1")
+	go hello(1, 5, done)
 	fmt.Println("Main going to call hello go goroutine 2")
 	go hello(2, 1, done)
 	y := <-done
 	fmt.Println("Main received data x", y)
+	fmt.Println("Calling 3")
+	go hello(3, 5, done)
+	fmt.Println("After calling 3")
+	y = <-done
+	fmt.Println("Main received data y", y)
+	fmt.Println("Before last done")
+	<-done
+	fmt.Println("After done")
+
+}
+
+func runThree() {
+	done := make(chan string)
+	fmt.Println("Main going to call hello go goroutine 1")
+	go hello(1, 5, done)
+	fmt.Println("Main going to call hello go goroutine 2")
+	go hello(2, 1, done)
+	y := <-done
+	fmt.Println("Main received data x", y)
+	fmt.Println("Calling 3")
+	go hello(3, 5, done)
+	fmt.Println("After calling 3")
 	y = <-done
 	fmt.Println("Main received data y", y)
 
+	// Why is 3 never received?
+	/*
+		By default channels are unbuffered, meaning that they will only accept
+		sends (chan <-) if there is a corresponding receive (<- chan) ready to receive the sent value.
+	*/
+}
+
+func runTwo() {
+	done := make(chan string)
+	fmt.Println("Main going to call hello go goroutine 1")
+	go hello(1, 2, done)
+	fmt.Println("Main going to call hello go goroutine 2")
+	go hello(2, 5, done)
+	x := <-done
+	fmt.Println("Main received data x", x)
+	y := <-done
+	fmt.Println("Main received data y", y)
+}
+
+func runOne() {
+	done := make(chan string)
+	fmt.Println("Main going to call hello go goroutine 1")
+	go hello(1, 5, done)
+	fmt.Println("Main going to call hello go goroutine 2")
+	go hello(2, 2, done)
+	x := <-done
+	fmt.Println("Main received data x", x)
+	y := <-done
+	fmt.Println("Main received data y", y)
 }
 
 func hello(num int, duration int, done chan string) {
